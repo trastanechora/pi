5f09caae3f788f1028ea26cd7459170fda7f227b
trastanechora <trastanechora@gmail.com>
2020-02-17 18:40:23 +0700

    Added quote card component

---
+++ components/QuoteCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center" @click="getRandomQuote">
+      <blockquote class="blockquote">
+        &#8220;{{ quote.content }}&#8221;
+        <footer>
+          <small>
+            <em>&mdash;{{ quote.author }}</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: {
+    quote: {
+      type: Object,
+      default: null
+    },
+    getRandomQuote: {
+      type: Function,
+      default: () => {}
+    }
+  }
+}
+</script>
