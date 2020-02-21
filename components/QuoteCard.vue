7124485eafb080f8be7cb304678207092e068a2f
trastanechora <trastanechora@gmail.com>
2020-02-22 02:50:54 +0700

    Modify qoute card component to parse paragraphs

--- components/QuoteCard.vue
+++ components/QuoteCard.vue
@@ -3,10 +3,13 @@
     <v-layout>
       <v-flex class="text-center" @click="getRandomQuote">
         <blockquote class="blockquote">
-          &#8220;{{ quote.content }}&#8221;
+          <!-- &#8220;{{ quote.content }}&#8221; -->
+          <p v-for="paragraph in quote.content" :key="paragraph.id">
+            {{ paragraph }}
+          </p>
           <footer>
             <small>
-              <em>&mdash;{{ quote.author }}</em>
+              <em>&mdash;{{ quote.author.name }}</em>
             </small>
           </footer>
         </blockquote>
@@ -26,6 +29,9 @@ export default {
       type: Function,
       default: () => {}
     }
+  },
+  mounted() {
+    console.warn('>>> Check quote: ', this.quote)
   }
 }
 </script>
