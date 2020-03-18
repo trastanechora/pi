10bfba6125181d293e55def77ccb7e93b7af7a35
trastanechora <trastanechora@gmail.com>
2020-03-18 07:12:03 +0700

    Initiate nuxt typescript

---
+++ nuxt.config.ts
@@ -0,0 +1,68 @@
+const isDev = process.env.NODE_ENV !== "production"
+
+export default {
+  mode: "spa",
+  modern: !isDev,
+  head: {
+    titleTemplate: "Portal Inspirasi | %s",
+    meta: [
+      {
+        property: "og:image",
+        content:
+          "https://user-images.githubusercontent.com/904724/26879447-689b56a8-4b91-11e7-968f-5eea1d6c71b4.png"
+      },
+      {
+        property: "twitter:card",
+        content: "summary_large_image"
+      },
+      {
+        property: "twitter:site",
+        content: "@nuxt_js"
+      }
+    ],
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico"
+      }
+    ]
+  },
+  loading: {
+    color: "#59cc93"
+  },
+  manifest: {
+    name: "Portal Inspirasi",
+    short_name: "PI",
+    description: "Portal Inspirasi app built with Nuxt.js & Typescript",
+    theme_color: "#188269"
+  },
+  modules: ["@nuxtjs/pwa", "@nuxtjs/component-cache", "@nuxtjs/axios"],
+  axios: {
+    debug: isDev,
+    proxy: true
+  },
+  proxy: {
+    "/api": {
+      target: "https://api.hnpwa.com/v0/",
+      pathRewrite: {
+        "^/api/": ""
+      }
+    }
+  },
+  plugins: ["~/plugins/filters"],
+  serverMiddleware: ["~/common/cache"],
+  render: {
+    http2: {
+      push: true
+    },
+    static: {
+      maxAge: "1y",
+      setHeaders(res, path) {
+        if (path.includes("sw.js")) {
+          res.setHeader("Cache-Control", `public, max-age=${15 * 60}`)
+        }
+      }
+    }
+  }
+}
