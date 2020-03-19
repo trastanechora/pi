d097a01a3ef5b0199fc3f2daf8a50542196aa3b6
trastanechora <trastanechora@gmail.com>
2020-03-19 14:40:04 +0700

    Added layout

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app dark>
+    <TopToolbar></TopToolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <BottomNav></BottomNav>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import TopToolbar from '../components/TopToolbar.vue'
+import BottomNav from '../components/BottomNav.vue'
+
+@Component({
+  components: {
+    TopToolbar,
+    BottomNav
+  }
+})
+export default class extends Vue {
+  created() {
+    console.warn('>>> Hello Typescript! <<<')
+  }
+}
+</script>
