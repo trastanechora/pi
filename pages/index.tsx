3f55c1fb8b781cb2682a90dc3659074dec6449c3
trastanechora <trastanechora@gmail.com>
2020-03-19 14:16:44 +0700

    Clean up and restart

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Vue, Component } from "vue-property-decorator"
-import { validFeeds } from "~/common/api"
-
-@Component({
-  fetch({ redirect }) {
-    redirect("/" + validFeeds[0])
-  }
-})
-export default class Index extends Vue { }
