0a4fb0bd077da1dfa76e7e9477128657c31160b9
trastanechora <trastanechora@gmail.com>
2020-03-20 22:13:59 +0700

    Added empty (only topToolbar) layout

---
+++ layouts/empty.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app dark>
+    <TopToolbar></TopToolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import TopToolbar from '../components/TopToolbar.vue'
+
+@Component({
+  name: "emptyLayout",
+  components: {
+    TopToolbar
+  }
+})
+export default class extends Vue {
+}
+</script>
